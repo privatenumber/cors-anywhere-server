2d0a7e590584f01eaa25d74af0e31611eb4a5004
Hiroki Osame <osame@squareup.com>
2020-02-12 11:12:55 -0500

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 8081;
+
+const cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], 
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
